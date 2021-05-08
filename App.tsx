439886ff947bebba97c90bc10c336225b276baa0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-08 14:02:09 -0300

    Criando pagina de boas vindas

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Welcome } from "./src/pages/Welcome";
 
 export default function App() {
-  return (
-    <View style={style.conatiner}>
-      <Text>Olá</Text>
-      <Text>Nlw#5</Text>
-    </View>
-  );
+  return <Welcome />;
 }
-
-const style = StyleSheet.create({
-  conatiner: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
