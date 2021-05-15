720675d17400df23ca9c7e77c50cefad0966f66e
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-15 13:46:28 -0300

    Criando compoente de lista

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { EnvariomentButton } from "../components/EnviromantButton";
 import { Header } from "../components/Header";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function PlantSelect() {
+  useEffect(() => {}, []);
+
   return (
     <View style={styles.conatiner}>
       <View style={styles.header}>
         <Header />
         <Text style={styles.title}>Em qual ambiente</Text>
         <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
+      <View>
+        <FlatList
+          data={[1, 2, 3, 4, 5]}
+          renderItem={(item) => (
+            <EnvariomentButton title="Cozinha" active={true} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
       </View>
     </View>
   );
@@ -38,5 +52,12 @@ const styles = StyleSheet.create({
     fontSize: 17,
     lineHeight: 20,
     color: colors.heading,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
 });
