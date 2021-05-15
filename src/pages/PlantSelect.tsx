9d394eec2b145e6164f8aa08ff6894afd23a67e4
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-15 12:21:16 -0300

    Criando titulo

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Header } from "../components/Header";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function PlantSelect() {
   return (
     <View style={styles.conatiner}>
-      <Header />
+      <View style={styles.header}>
+        <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
     </View>
   );
 }
@@ -17,5 +22,21 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
     // justifyContent: "center",
     // alignItems: "center",
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
   },
 });
